21c2d60b3b774a68823362b3a9cc2308b4cf84d9
vzytoi <cyprien.henner@icloud.com>
2021-08-25 09:49:48 +0200

    you can see now that treesitter work (;

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,8 @@
 require('nvim-treesitter.configs').setup({
     ensure_installed = "maintained",
     highlight = {
-        enable = true
+        enable = true,
+        use_language_tree = true
     },
     indent = {
         enable = true
@@ -9,7 +10,27 @@ require('nvim-treesitter.configs').setup({
     autopairs = {
         enable = true
     },
-    autotag = {
-        enable = true
-    }
+    textobjects = {
+        select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["al"] = "@loop.outer",
+                ["il"] = "@loop.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner"
+            },
+        },
+        swap = {
+            enable = true,
+            swap_next = {
+                ["<leader>a"] = "@parameter.inner",
+            },
+            swap_previous = {
+                ["<leader>A"] = "@parameter.inner",
+            },
+        },
+    },
 })
