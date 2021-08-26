34efe32b2c7ce0522f2b8e03e3450b7516cbfdef
vzytoi <cyprien.henner@icloud.com>
2021-08-26 21:31:24 +0200

    restore last cursor position (yes bb)
    + session it going to be a realy pleasure

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,4 +1,5 @@
 vim.cmd [[
-    autocmd BufWritePost plugins.lua source $MYVIMRC | PackerCompile
-    autocmd! TermOpen term://* lua set_terminal_keymaps()
+autocmd! BufWritePost plugins.lua source $MYVIMRC | PackerCompile
+autocmd! TermOpen term://* lua set_terminal_keymaps()
+au BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") && &filetype != "gitcommit" | execute("normal `\"") | endif
 ]]
