d8f7df81858c5b52c2a9aacbe7146213a85da55a
vzytoi <cyprien.henner@icloud.com>
2021-08-26 16:41:31 +0200

    adding a mapping for help_tags

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,7 +7,8 @@ function M.setup()
             { 'f', '<Cmd>Telescope git_files theme=get_ivy<cr>' },
             { 'f', {
                 { 'g', '<Cmd>Telescope live_grep theme=get_ivy<CR>' },
-                { 'b', '<Cmd>Telescope buffers theme=get_ivy<CR>' }
+                { 'b', '<Cmd>Telescope buffers theme=get_ivy<CR>' },
+                { 'h', '<Cmd>Telescope help_tags theme=get_ivy<CR>' }
             }},
         }}
     }
