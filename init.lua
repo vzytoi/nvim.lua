e04ec6554c117bdbcb149875d81fad089385232d
vzytoi <cyprien.henner@icloud.com>
2021-08-27 23:58:06 +0200

    more pretty init

--- init.lua
+++ init.lua
@@ -1,3 +1,15 @@
-require('options')
-require('autocmd')
-require('plugins')
+function load_files()
+
+    local files = {
+        { 'options', false },
+        { 'autocmd', false },
+        { 'plugins', false }
+    }
+
+    for _, f in ipairs(files) do
+        if not f[2] then
+            require(f[1])
+        end
+    end
+
+end
