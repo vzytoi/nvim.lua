e5e8bb3d5c0270e4af1f605049087c926b60f4e7
vzytoi <cyprien.henner@icloud.com>
2021-08-27 23:39:20 +0200

    formatting

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,5 +1,3 @@
-vim.cmd [[
-autocmd! BufWritePost plugins.lua source $MYVIMRC | PackerCompile
-autocmd! TermOpen term://* lua set_terminal_keymaps()
-au BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") && &filetype != "gitcommit" | execute("normal `\"") | endif
-]]
+vim.cmd('autocmd! BufWritePost plugins.lua source $MYVIMRC | PackerCompile')
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.cmd[[au BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") && &filetype != "gitcommit" | execute("normal `\"") | endif]]
