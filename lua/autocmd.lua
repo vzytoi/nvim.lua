149fe2aa590d4d862e984e7a1f9455a3423ecbad
vzytoi <cyprien.henner@icloud.com>
2021-08-29 01:39:23 +0200

    better autocmd management

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,7 +1,37 @@
-vim.cmd([[
-    au BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") && &filetype != "gitcommit" | execute("normal `\"") | endif
-    autocmd FileType * set fo-=c fo-=r fo-=o fo+=j
-    autocmd! BufWritePost plugins.lua source $MYVIMRC | PackerCompile
-    autocmd! TermOpen term://* lua set_terminal_keymaps()
-    autocmd BufEnter * silent! lcd %:p:h
-]])
+local autocmds = {
+    _general = {
+        { "BufRead", "*", [[ if line("'\"") > 0 && line("'\"") <= line("$") | exe "normal g`\"" | endif ]] },
+        { "BufEnter", "*", "silent! lcd %:p:h" },
+        { "BufWinEnter", "*", "setlocal formatoptions-=c formatoptions-=r formatoptions-=o" },
+        { "BufRead", "*", "setlocal formatoptions-=c formatoptions-=r formatoptions-=o" },
+    },
+    _git = {
+        { "FileType", "gitcommit", "setlocal wrap" },
+        { "FileType", "gitcommit", "setlocal spell" },
+    },
+    _markdown = {
+        { "FileType", "markdown", "setlocal wrap" },
+        { "FileType", "markdown", "setlocal spell" },
+    },
+    _auto_resize = {
+        { "VimResized", "*", "wincmd =" },
+    },
+    _packer_compile = {
+        { "BufWritePost", "plugins.lua", "PackerCompile" },
+    },
+    _term = {
+        { "TermOpen", "term://*", "lua set_terminal_keymaps()"}
+    }
+}
+
+for gp, definition in pairs(autocmds) do
+    vim.cmd("augroup " .. gp)
+    vim.cmd "autocmd!"
+
+    for _, def in pairs(definition) do
+        local cmd = table.concat(vim.tbl_flatten { "autocmd", def }, " ")
+        vim.cmd(cmd)
+    end
+
+    vim.cmd "augroup END"
+end
