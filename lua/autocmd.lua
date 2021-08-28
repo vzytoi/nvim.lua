efa2e6d6004071bcb35b1409a5e34c5dbc5598bd
vzytoi <cyprien.henner@icloud.com>
2021-08-28 19:08:24 +0200

    auto pwd

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -3,4 +3,5 @@ vim.cmd([[
     autocmd FileType * set fo-=c fo-=r fo-=o fo+=j
     autocmd! BufWritePost plugins.lua source $MYVIMRC | PackerCompile
     autocmd! TermOpen term://* lua set_terminal_keymaps()
+    autocmd BufEnter * silent! lcd %:p:h
 ]])
