cac308f40154b7ce1f34613b2b2bba464654e0b2
vzytoi <cyprien.henner@icloud.com>
2021-09-01 22:43:26 +0200

    add keymap to naviguate between diagnostics

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -11,6 +11,8 @@ function M.setup()
                 { 'd', '<Plug>(coc-definition)', options = { noremap = false } },
                 { 'f', '<Plug>(coc-references)', options = { noremap = false } },
                 { 'r', '<Plug>(coc-rename)', options = { noremap = false } },
+                { 'j', '<Plug>(coc-diagnostic-next)', options = { noremap = false } },
+                { 'k', '<Plug>(coc-diagnostic-prev)', options = { noremap = false } }
             }},
             { 'e', ':CocCommand explorer<cr>' }
         }}
