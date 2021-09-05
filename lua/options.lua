0eab90cd5f5b7e739e231097d8387083be55cac0
vzytoi <cyprien.henner@icloud.com>
2021-09-05 21:59:19 +0200

    made a loop to highlight

--- lua/options.lua
+++ lua/options.lua
@@ -1,19 +1,52 @@
-vim.cmd [[
-    let g:loaded_python_provider = 0
-    let g:python3_host_prog = '~\AppData\Local\Programs\Python\Python39\python.exe'
-]]
+function DisableBuiltin()
 
-local plug_buitlins = {
-    'gzip', 'zip', 'zipPlugin', 'tar', 'tarPlugins',
-    'getscript', 'getscriptPlugin', 'vimball', 'vimballPlugin', '2html_plugin',
-    'matchit', 'matchiparen', 'logiPat', 'rrhelper',
-    'netrw', 'netrwPlugin', 'netrwSettings', 'remote_plugins', 'man',
-    'shada_plugin', 'spellfile_plugin', 'tutor_mode_plugin'
-}
+    local plug_buitlins = {
+        'gzip', 'zip', 'zipPlugin', 'tar', 'tarPlugins',
+        'getscript', 'getscriptPlugin', 'vimball', 'vimballPlugin', '2html_plugin',
+        'matchit', 'matchiparen', 'logiPat', 'rrhelper',
+        'netrw', 'netrwPlugin', 'netrwSettings', 'remote_plugins', 'man',
+        'shada_plugin', 'spellfile_plugin', 'tutor_mode_plugin'
+    }
 
-for _, p in pairs(plug_buitlins) do
-    vim.g["loaded_" .. p] = 1
+    for _, p in pairs(plug_buitlins) do
+        vim.g["loaded_" .. p] = 1
+    end
+
 end
+
+function HighlightFuction()
+
+    local hi = {
+        ["Normal"] = {
+            guifg = "#EEEEEE", guibg = "#0a0a0a", gui = "none" },
+        ["NonText"] = {
+            guifg = '#6B6B6B', guibg = 'none', gui = 'none' },
+        ["SignColumn"] = {
+            guifg = 'none', guibg = 'none', gui = 'none', },
+        ["LineNr"] = {
+            guifg = '#6B6B6B', guibg='none', gui='none' },
+        ["MatchParen"] = {
+            guifg = '#F0D50C', guibg='none', gui='none' },
+        ["VertSplit"] = {
+            guifg = '#6B6B6B', guibg='none', gui='none' },
+        ["CocUnusedHighlight"] = {
+            guibg = 'none', guifg='none', gui='underline' },
+        ["CocHintHighlight"] = {
+            cterm = 'undercurl', guisp='#000000', }
+    }
+
+    for m, t in pairs(hi) do
+        local s = 'hi ' .. m
+        for k, v in pairs(t) do
+            s = s .. ' ' .. table.concat({k,v}, '=')
+        end
+        vim.cmd(s)
+    end
+
+end
+
+DisableBuiltin()
+HighlightFuction()
 
 vim.o.sessionoptions="blank,buffers,curdir,folds,help,options,tabpages,winsize,resize,winpos,terminal"
 
@@ -57,18 +90,13 @@ set.scrolloff = 8
 
 set.undofile = true
 
-vim.cmd('hi Normal guifg=#EEEEEE guibg=#0a0a0a gui=none')
-vim.cmd('hi NonText guifg=#6B6B6B guibg=none gui=none')
-vim.cmd('hi SignColumn guifg=none guibg=none gui=none')
-vim.cmd('hi LineNr guifg=#6B6B6B guibg=none gui=none')
-vim.cmd('hi MatchParen guifg=#F0D50C guibg=none gui=none')
-vim.cmd('hi VertSplit guifg=#6B6B6B guibg=none gui=none')
-vim.cmd('hi CocUnusedHighlight guibg=none guifg=none gui=underline')
-vim.cmd('hi CocHintHighlight cterm=undercurl guisp=#000000')
-vim.cmd('hi CocHintHighlight cterm=undercurl guisp=#000000')
-
 vim.cmd [[
 let g:ycm_key_list_select_completion = ['<TAB>', '<Down>', '<C-j>', '<C-n>']
 let g:ycm_key_list_previous_completion = ['<S-TAB>', '<Up>', '<C-k>', '<C-p>']
 let g:ycm_key_list_stop_completion = ['<C-y>', '<Cr>']
+]]
+
+vim.cmd [[
+let g:loaded_python_provider = 0
+let g:python3_host_prog = '~\AppData\Local\Programs\Python\Python39\python.exe'
 ]]
