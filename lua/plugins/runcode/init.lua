c3d2b20a74208827ae746eb2ffb141b96a87de6a
vzytoi <cyprien.henner@icloud.com>
2021-11-02 01:31:22 +0100

    make runcode splits resize automatically and make my own Meaure-Command ouput

--- lua/plugins/runcode/init.lua
+++ lua/plugins/runcode/init.lua
@@ -5,33 +5,94 @@ function M.setup()
     local map = {
         { '<leader>', {
             { 'x', function()
-                vim.api.nvim_command("!" .. M.command())
+                M.run('x')
             end },
             { 'x', {
                 { 's', function()
-                    vim.api.nvim_command(
-                        "split_f|r !" .. M.command()
-                    )
-                    vim.api.nvim_command("res -20")
-                    require("options").RunCodeBuffer()
+                    M.run('s')
                 end },
                 { 'v', function()
-                    vim.api.nvim_command(
-                        "vnew|r !" .. M.command()
-                    )
-                    vim.api.nvim_command("vert res -60")
-                    require("options").RunCodeBuffer()
+                    M.run('v')
                 end },
                 { 'e', function()
-                    vim.api.nvim_command(
-                        "!Measure-Command{" .. M.command() .. "}"
-                    )
+                    M.time()
                 end }
             }},
         }}
     }
 
     return map
+
+end
+
+function M.run(dir)
+
+    local d = {
+        s = "split_f|r !",
+        v = "vnew|r !",
+        x = "!"
+    }
+
+    vim.api.nvim_command(
+        d[dir] .. M.command()
+    )
+
+end
+
+function M.time()
+
+    local d = {
+        "Milliseconds", "Seconds", "Minutes"
+    }
+
+    local s
+    local f
+
+    for _, v in pairs(d) do
+        s =
+        vim.fn.system(
+            string.format(
+                "((Measure-Command{%s}).Total%s).ToString()",
+                M.command(), v
+            )
+        )
+        if tonumber(string.sub(s, 1, 1)) > 0 then
+            f = v
+            break
+        end
+    end
+
+    print(
+        string.format(
+            "%s %s",
+            string.gsub(s, "[\r\n]", ""), f
+        )
+    )
+
+end
+
+function M.resize(dir)
+    dir = dir or ""
+
+    local lines = vim.fn.getline(1, '$')
+    local m = 0
+
+    if string.len(dir) == 0 then
+        m = #lines
+    else
+        for _, v in pairs(lines) do
+            if string.len(v) > m then
+                m = string.len(v)
+            end
+        end
+        if m > 150 then
+            m = 75
+        end
+    end
+
+    vim.api.nvim_command(
+        string.format("%s res %s", dir, m + 10)
+    )
 
 end
 
