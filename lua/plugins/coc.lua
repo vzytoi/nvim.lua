a5ef4bebc07dbc28d80d280b8c00455b513911df
vzytoi <cyprien.henner@icloud.com>
2021-11-21 19:13:31 +0100

    remove old stuffs & make cocexplorer open the right side

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -1,5 +1,16 @@
 local M = {}
 
+local function open(pos)
+
+    vim.api.nvim_command(
+        string.format("CocCommand explorer --position %s",
+        pos)
+    )
+
+    vim.g.side = pos
+
+end
+
 function M.setup()
 
     local map = {
@@ -20,7 +31,24 @@ function M.setup()
                     { 'i', '<Plug>(coc-implementation)' },
                     { 't', '<Plug>(coc-type-definition)' },
                 }},
-                { 'e', ':CocCommand explorer<cr>'}
+                { 'e', function()
+
+                    if vim.g.explorer_is_open == 1 then
+                        vim.api.nvim_command(
+                            'CocCommand explorer --position ' .. vim.g.side
+                        )
+                        return false
+                    end
+
+                    local wcount = vim.fn.winnr('$')
+                    local wcurr = vim.fn.winnr()
+
+                    open(
+                        (wcurr > math.ceil(wcount / 2) or wcurr == wcount)
+                        and 'right' or 'left'
+                    )
+
+                end}
             }}
         }}
     }
