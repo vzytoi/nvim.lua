1ccbee980ab960e5114a7d6034b35b7f79f5f682
vzytoi <65293868+vzytoi@users.noreply.github.com>
2022-12-05 23:00:18 +0100

    Delete tests directory

--- tests/deviocns.lua
+++
@@ -1,4 +0,0 @@
-local devicons = require('nvim-web-devicons')
-local icon = devicons.get_icon('tree')
-
-print(icon)
--- tests/pow.ml
+++
@@ -1,3 +0,0 @@
-let rec pow a = function
-  | 0 -> 1
-  | n -> a * pow a (n - 1)
